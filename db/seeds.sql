c8bfa9ba3f64248312a4c0c81c816dc52ac74c1d
Zachary Binx <72980919+ZacharyBinx@users.noreply.github.com>
2021-01-03 23:31:08 -0700

    functioning buttons for eating and adding burgers. styling needed

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Jalapeno Bacon Burger');
+INSERT INTO burgers (name) VALUES ('Heart Attack Burger');
+INSERT INTO burgers (name, eaten) VALUES ('Southwestern Bacon BBQ', true);
+INSERT INTO burgers (name, eaten) VALUES ('Smokehouse Stack', true);
+INSERT INTO burgers (name, eaten) VALUES ('2x2 Animal Style', true);
+INSERT INTO burgers (name) VALUES ('McDouble');
