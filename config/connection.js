4a2001e047d6d0e3eb93a4948b88b529cd0e58ad
Zachary Binx <72980919+ZacharyBinx@users.noreply.github.com>
2021-01-12 00:45:58 -0700

    deploying

--- config/connection.js
+++ config/connection.js
@@ -10,8 +10,8 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         port: 3306,
         host: 'localhost',
-        user: 'root',
-        password: 'root',
+        user: process.env.USER,
+        password: process.env.PASSWORD,
         database: 'burger_db'
     })
 };
